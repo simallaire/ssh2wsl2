2d94a8bfb1712afd97ec3d984d2c2be3ec98f102
Simon-Pierre Allaire <spallaire@distech-controls.com>
2020-12-11 01:34:17 -0500

    Add comments explaining each step

--- logon.bat
+++ logon.bat
@@ -1,7 +1,12 @@
+:: Get the local IP of wsl2 and save it to a temp fiLe
 (wsl ip addr | wsl grep eth0 | wsl grep inet | wsl cut -d" " -f6 | wsl cut -d"/" -f1 )>tmp
+:: Set a variable with the ip addr
 set /p ipadd=<tmp
 echo %ipadd%
+:: Forward the port 2222 
 netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2222 connectaddress=%ipadd% connectport=2222
+:: Add an exception in windows firewall
 netsh advfirewall firewall add rule name="Open Port 2222 for WSL2" dir=in action=allow protocol=TCP localport=2222
+:: Start ssh as root in wsl
 wsl -u root sudo service ssh start
 
