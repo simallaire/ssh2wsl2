f3c96d88bb269f5fe8a87f49220717fcc5f009c3
simallaire <sp.allaire93@gmail.com>
2022-09-16 23:45:41 -0400

    Update logon.bat

--- logon.bat
+++ logon.bat
@@ -5,8 +5,6 @@ set /p ipadd=<tmp
 echo %ipadd%
 :: Forward the port 2222 
 netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2222 connectaddress=%ipadd% connectport=2222
-:: Add an exception in windows firewall
-netsh advfirewall firewall add rule name="Open Port 2222 for WSL2" dir=in action=allow protocol=TCP localport=2222
 :: Start ssh as root in wsl
 wsl -u root sudo service ssh start
 
